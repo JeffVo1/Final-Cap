875c7db775845578c705618d2b46afc293a84507
Jenna Parker <parkerjennae@gmail.com>
2021-12-04 15:40:22 -0500

    Added charity controller, updated Charityjdbc, updtated database

---
+++ java/src/main/java/com/techelevator/controller/CharityController.java
@@ -0,0 +1,28 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.CharityDao;
+import com.techelevator.model.Charity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CharityController {
+
+    private CharityDao charityDao;
+    public CharityController(CharityDao charityDao) {
+        this.charityDao = charityDao;
+    }
+    @RequestMapping(path = "/charity/{charityName}", method = RequestMethod.GET)
+    public Charity getCharity(@PathVariable String charityName) {
+        return charityDao.getCharityByName(charityName);
+    }
+    @RequestMapping(path = "/charity/update", method = RequestMethod.PUT)
+    public void updateCharity(@RequestBody Charity charityName) {
+        charityDao.updateCharity(charityName);
+    }
+
+    @RequestMapping(path = "/charity", method = RequestMethod.POST)
+    public void createCharity(@RequestBody Charity charityName) {
+        charityDao.createCharity(charityName);
+    }
+
+}
--- java/src/main/java/com/techelevator/dao/JdbcCharityDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcCharityDao.java
@@ -1,20 +1,49 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Charity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
 
 public class JdbcCharityDao implements CharityDao{
+
+   private JdbcTemplate jdbcTemplate;
+
+   public JdbcCharityDao(JdbcTemplate jdbcTemplate) {
+       this.jdbcTemplate = jdbcTemplate;
+   }
+
+
     @Override
-    public boolean createCharity(Charity charity) {
-        return false;
+    public void createCharity(Charity charity) {
+       String sql = "INSERT INTO charity (charity_name) " +
+               "VALUES (?);";
+       jdbcTemplate.update(sql, charity.getCharityName());
     }
 
     @Override
-    public boolean updateCharity(Charity charity) {
-        return false;
+    public void updateCharity(Charity charity) {
+       String sql = "UPDATE charity " +
+               "SET charity_id = ?, charity_name = ? " +
+               "WHERE charity_id = ?";
+       jdbcTemplate.update(sql, charity.getCharityId(), charity.getCharityName(), charity.getCharityId());
     }
+
 
     @Override
     public Charity getCharityByName(String charityName) {
+        String sql = "SELECT charity_name, charity_id " +
+                "FROM charity " +
+                "WHERE charity_name = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, charityName);
+        if (results.next()) {
+            Charity charity = new Charity();
+            charity.setCharityName(results.getString("charity_name"));
+            charity.setCharityId(results.getFloat("charity_id"));
+            return charity;
+        }
         return null;
     }
 }
